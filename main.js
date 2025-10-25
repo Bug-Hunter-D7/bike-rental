ed76fdde4c592a94a25c9a648e53be13fb9c4172
DHINESH <101002879+Bug-Hunter-D7@users.noreply.github.com>
2025-10-25 04:15:49 +0000

    Initial commit - Bike-Rental

---
+++ main.js
@@ -0,0 +1,22 @@
+// deals
+const tabs = document.querySelector(".deals__tabs");
+
+tabs.addEventListener("click", (e) => {
+  const tabContents = document.querySelectorAll(
+    ".deals__container .tab__content"
+  );
+  Array.from(tabs.children).forEach((item) => {
+    if (item.dataset.id === e.target.dataset.id) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+  tabContents.forEach((item) => {
+    if (item.id === e.target.dataset.id) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+});
